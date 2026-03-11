a01bd7ff1ea48cb96a879dbe20595fb839e35c4e
NguyenHoangDangKhoaCNTT2 <khoa.nhd.65cntt@ntu.edu.vn>
2026-03-11 16:58:41 +0700

    thuchanh2_2

--- BaiTH2_1/app/src/main/java/khoa/edu/appcong/MainActivity.java
+++ BaiTH2_1/app/src/main/java/khoa/edu/appcong/MainActivity.java
@@ -1,6 +1,8 @@
 package khoa.edu.appcong;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    // Bộ lắng nghe và xử lý sự kiện click lên nút tính tổng
+    public void XuLyCong(View view) {
+        // Tìm,tham chiếu đến điều khiển trên tệp XML, mapping sang java file
+        EditText editTestSoA = findViewById(R.id.edtA);
+        EditText editTestSoB = findViewById(R.id.edtB);
+        EditText editTestKetQua = findViewById(R.id.edtKQ);
+        // Lấy dữ liệu số A
+        String strA = editTestSoA.getText().toString();
+        // Lấy dữ liệu số B
+        String strB = editTestSoB.getText().toString();
+        // Chuyển dữ liệu sang số
+        int so_A = Integer.parseInt(strA);
+        int so_B = Integer.parseInt(strB);
+        // Tính toán theo yêu cầu
+        int tong = so_A + so_B;
+        String strTong = String.valueOf(tong);     // Chuyển sang dạng chuỗi
+        // Hiện ra màn hình
+        editTestKetQua.setText(strTong);
     }
 }
