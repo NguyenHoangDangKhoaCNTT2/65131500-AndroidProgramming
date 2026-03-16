b25bf82bc53f54ae2d170cc4a7d747d0299b8e1f
NguyenHoangDangKhoaCNTT2 <khoa.nhd.65cntt@ntu.edu.vn>
2026-03-16 19:34:20 +0700

    baiTH

---
+++ BaiTH5_XuLySuKien1/app/src/main/java/khoa/edu/baith5/MainActivity.java
@@ -0,0 +1,102 @@
+package khoa.edu.baith5;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    //Khai báo các đối tượng gắn với đkhien tương ứng
+    EditText editTextSo1;
+    EditText editTextSo2;
+    EditText editTextKQ;
+    Button nutCong, nutTru, nutNhan, nutChia;
+    void TimDieuKhien() {
+        editTextSo1 = (EditText) findViewById(R.id.edtSo1);
+        editTextSo2 = (EditText) findViewById(R.id.edtSo2);
+        editTextKQ = (EditText) findViewById((R.id.edtKetQua));
+        nutCong = (Button) findViewById(R.id.btnCong);
+        nutTru = (Button) findViewById(R.id.btnTru);
+        nutNhan = (Button) findViewById(R.id.btnNhan);
+        nutChia = (Button) findViewById(R.id.btnChia);
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        TimDieuKhien();
+        //Bộ lắng nghe và code xử lý
+        View.OnClickListener boLangNgheCong = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                XULY_CONG();
+            }
+        };
+        nutCong.setOnClickListener(boLangNgheCong);
+
+        nutTru.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                XULY_TRU();
+            }
+        });
+
+        nutNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                XULY_NHAN();
+            }
+        });
+
+        nutChia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                XULY_CHIA();
+            }
+        });
+    }
+    void XULY_CONG() {
+        String so1 = editTextSo1.getText().toString();
+        String so2 = editTextSo2.getText().toString();
+        float num1 = Float.parseFloat(so1);
+        float num2 = Float.parseFloat(so2);
+        float tong = num1 + num2;
+        String chuoiKQ = String.valueOf(tong);
+        editTextKQ.setText(chuoiKQ);
+    }
+    void XULY_TRU() {
+        String so1 = editTextSo1.getText().toString();
+        String so2 = editTextSo2.getText().toString();
+        float num1 = Float.parseFloat(so1);
+        float num2 = Float.parseFloat(so2);
+        float hieu = num1 - num2;
+        String chuoiKQ = String.valueOf(hieu);
+        editTextKQ.setText(chuoiKQ);
+    }
+    void XULY_NHAN() {
+        String so1 = editTextSo1.getText().toString();
+        String so2 = editTextSo2.getText().toString();
+        float num1 = Float.parseFloat(so1);
+        float num2 = Float.parseFloat(so2);
+        float tich = num1 * num2;
+        String chuoiKQ = String.valueOf(tich);
+        editTextKQ.setText(chuoiKQ);
+    }
+    void XULY_CHIA(){
+        String so1 = editTextSo1.getText().toString();
+        String so2 = editTextSo2.getText().toString();
+        float num1 = Float.parseFloat(so1);
+        float num2 = Float.parseFloat(so2);
+        float thuong = num1 / num2;
+        String chuoiKQ = String.valueOf(thuong);
+        editTextKQ.setText(chuoiKQ);
+    }
+}
---
+++ BaiTH6_XuLySuKien_TinhTong/app/src/main/java/khoa/edu/baith6/MainActivity.java
@@ -0,0 +1,39 @@
+package khoa.edu.baith6;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    // Bộ lắng nghe và xử lý sự kiện click lên nút tính tổng
+    public void XuLyCong(View view) {
+        // Tìm,tham chiếu đến điều khiển trên tệp XML, mapping sang java file
+        EditText editTestSoA = findViewById(R.id.edtA);
+        EditText editTestSoB = findViewById(R.id.edtB);
+        EditText editTestKetQua = findViewById(R.id.edtKQ);
+        // Lấy dữ liệu số A
+        String strA = editTestSoA.getText().toString();
+        // Lấy dữ liệu số B
+        String strB = editTestSoB.getText().toString();
+        // Chuyển dữ liệu sang số
+        int so_A = Integer.parseInt(strA);
+        int so_B = Integer.parseInt(strB);
+        // Tính toán theo yêu cầu
+        int tong = so_A + so_B;
+        String strTong = String.valueOf(tong);     // Chuyển sang dạng chuỗi
+        // Hiện ra màn hình
+        editTestKetQua.setText(strTong);
+    }
+}
