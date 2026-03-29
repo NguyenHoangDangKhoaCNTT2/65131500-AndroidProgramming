e51a40ab0f1f1be6094a4c0fdd01b5ef6f9fa77d
NguyenHoangDangKhoaCNTT2 <khoa.nhd.65cntt@ntu.edu.vn>
2026-03-29 18:00:38 +0700

    BaiTH

---
+++ BaiTH9_Recyclerview/app/src/main/java/khoa/edu/baith9/LandScape.java
@@ -0,0 +1,26 @@
+package khoa.edu.baith9;
+
+public class LandScape {
+    String landImageFileName;
+    String landCaption;
+    public LandScape(String landImageFileName, String landCaption) {
+        this.landImageFileName = landImageFileName;
+        this.landCaption = landCaption;
+    }
+
+    public String getLandImageFileName() {
+        return landImageFileName;
+    }
+
+    public void setLandImageFileName(String landImageFileName) {
+        this.landImageFileName = landImageFileName;
+    }
+
+    public String getLandCaption() {
+        return landCaption;
+    }
+
+    public void setLandCaption(String landCaption) {
+        this.landCaption = landCaption;
+    }
+}
---
+++ BaiTH9_Recyclerview/app/src/main/java/khoa/edu/baith9/MainActivity.java
@@ -0,0 +1,52 @@
+package khoa.edu.baith9;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    LandScapeAdapter landScapeAdapter;
+    ArrayList<LandScape> recylerViewDatas;
+    RecyclerView recyclerViewLandscape;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //3
+        recylerViewDatas = getDataForRecyclerView();
+        //4
+        recyclerViewLandscape = findViewById(R.id.recyclerLand);
+        //5
+        // RecyclerView.LayoutManager layoutLinear = new LinearLayoutManager(this);
+        // recyclerViewLandscape.setLayoutManager(layoutLinear);
+
+        // RecyclerView.LayoutManager layoutLinearHorizonal = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        // recyclerViewLandscape.setLayoutManager(layoutLinearHorizonal);
+
+        RecyclerView.LayoutManager layoutGrid = new GridLayoutManager(this, 2);
+        recyclerViewLandscape.setLayoutManager(layoutGrid);
+        //6
+        landScapeAdapter = new LandScapeAdapter(this, recylerViewDatas);
+        //7
+        recyclerViewLandscape.setAdapter(landScapeAdapter);
+    }
+
+    ArrayList<LandScape> getDataForRecyclerView() {
+        ArrayList<LandScape> dsDuLieu = new ArrayList<LandScape>();
+        LandScape landScape1 = new LandScape("hanoi", "Cột cờ HÀ Nội");
+        dsDuLieu.add(landScape1);
+        dsDuLieu.add( new LandScape("eiffel", "Tháp Eiffel" ) );
+        dsDuLieu.add( new LandScape("buckingham", "Cung điện Buckingham" ) );
+        dsDuLieu.add( new LandScape("soliberty", "Tượng Nữ thần tự do" ) );
+        return dsDuLieu;
+    }
+}
