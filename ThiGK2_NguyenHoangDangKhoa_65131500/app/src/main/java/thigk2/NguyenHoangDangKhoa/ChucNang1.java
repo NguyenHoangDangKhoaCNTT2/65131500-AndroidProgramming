36d3979147f2c07e25cc5a190f0076c9a79f7e27
NguyenHoangDangKhoaCNTT2 <khoa.nhd.65cntt@ntu.edu.vn>
2026-04-20 07:50:30 +0700

    LẦN THỨ 2

--- ThiGK2_NguyenHoangDangKhoa_65131500/app/src/main/java/thigk2/NguyenHoangDangKhoa/ChucNang1.java
+++ ThiGK2_NguyenHoangDangKhoa_65131500/app/src/main/java/thigk2/NguyenHoangDangKhoa/ChucNang1.java
@@ -1,24 +1,32 @@
 package thigk2.NguyenHoangDangKhoa;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class ChucNang1 extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_chuc_nang1);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        EditText edtTien = findViewById(R.id.edtTien);
+        EditText edtTiGia = findViewById(R.id.edtTiGia);
+        Button btnTinh = findViewById(R.id.btnTinh);
+        TextView tvKetQua = findViewById(R.id.tvKetQua);
+
+        btnTinh.setOnClickListener(v -> {
+            try {
+                double tien = Double.parseDouble(edtTien.getText().toString());
+                double tiGia = Double.parseDouble(edtTiGia.getText().toString());
+                double ketQua = tien * tiGia;
+                tvKetQua.setText("Kết quả: " + ketQua + " VNĐ");
+            } catch (Exception e) {
+                Toast.makeText(this, "Vui lòng nhập số hợp lệ!", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
--- ThiGK2_NguyenHoangDangKhoa_65131500/app/src/main/java/thigk2/NguyenHoangDangKhoa/MainActivity.java
+++ ThiGK2_NguyenHoangDangKhoa_65131500/app/src/main/java/thigk2/NguyenHoangDangKhoa/MainActivity.java
@@ -1,24 +1,25 @@
 package thigk2.NguyenHoangDangKhoa;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Button btn1 = findViewById(R.id.btn1);
+        Button btn2 = findViewById(R.id.btn2);
+        Button btn3 = findViewById(R.id.btn3);
+        Button btn4 = findViewById(R.id.btn4);
+
+        btn1.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, ChucNang1.class)));
+        btn2.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, ChucNang2.class)));
+        btn3.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, ChucNang3.class)));
+        btn4.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, ChucNang4.class)));
     }
 }
