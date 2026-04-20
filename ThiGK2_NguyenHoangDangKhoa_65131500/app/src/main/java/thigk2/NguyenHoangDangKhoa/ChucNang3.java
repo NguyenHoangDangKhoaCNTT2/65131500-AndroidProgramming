d859daac5b436be3ecaae66f663ae684d2e46777
NguyenHoangDangKhoaCNTT2 <khoa.nhd.65cntt@ntu.edu.vn>
2026-04-20 08:22:48 +0700

    LẦN THỨ 5

--- ThiGK2_NguyenHoangDangKhoa_65131500/app/src/main/java/thigk2/NguyenHoangDangKhoa/ChucNang3.java
+++ ThiGK2_NguyenHoangDangKhoa_65131500/app/src/main/java/thigk2/NguyenHoangDangKhoa/ChucNang3.java
@@ -1,24 +1,57 @@
 package thigk2.NguyenHoangDangKhoa;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import java.util.ArrayList;
 
 public class ChucNang3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_chuc_nang3);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        ListView listViewDanhNhan = findViewById(R.id.listViewDanhNhan); // Xem file activity_chuc_nang3.xml có ID này chưa nhé
+        ArrayList<DanhNhan> list = new ArrayList<>();
+
+        // Truyền thẳng ảnh vào đây (nhớ thay hinh1, hinh2 bằng tên ảnh thật của mày trong drawable)
+        list.add(new DanhNhan("Trần Hưng Đạo", "Nam Định", R.drawable.hinh1));
+        list.add(new DanhNhan("Nguyễn Trãi", "Hải Dương", R.drawable.hinh2));
+        list.add(new DanhNhan("Võ Nguyên Giáp", "Quảng Bình", R.drawable.hinh3));
+        list.add(new DanhNhan("Quang Trung", "Bình Định", R.drawable.hinh4));
+        list.add(new DanhNhan("Hồ Chí Minh", "Nghệ An", R.drawable.hinh5));
+
+        // Viết gộp Adapter ngay tại đây luôn cho lẹ
+        ArrayAdapter<DanhNhan> adapter = new ArrayAdapter<DanhNhan>(this, R.layout.item_danh_nhan, list) {
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                if (convertView == null) {
+                    convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_danh_nhan, parent, false);
+                }
+
+                TextView txtTen = convertView.findViewById(R.id.txtTen);
+                TextView txtQueQuan = convertView.findViewById(R.id.txtQueQuan);
+                ImageView imgHinh = convertView.findViewById(R.id.imgHinh); // Ánh xạ ảnh
+
+                DanhNhan dn = list.get(position);
+                txtTen.setText(dn.ten);
+                txtQueQuan.setText("Quê quán: " + dn.queQuan);
+                imgHinh.setImageResource(dn.hinh); // Set ảnh thật vào đây!
+
+                return convertView;
+            }
+        };
+
+        listViewDanhNhan.setAdapter(adapter);
     }
 }
--- ThiGK2_NguyenHoangDangKhoa_65131500/app/src/main/java/thigk2/NguyenHoangDangKhoa/DanhNhan.java
+++ ThiGK2_NguyenHoangDangKhoa_65131500/app/src/main/java/thigk2/NguyenHoangDangKhoa/DanhNhan.java
@@ -1,17 +1,13 @@
 package thigk2.NguyenHoangDangKhoa;
 
 public class DanhNhan {
-    private String ten;
-    private String queQuan;
-    private int hinh;
+    String ten;
+    String queQuan;
+    int hinh;
 
     public DanhNhan(String ten, String queQuan, int hinh) {
         this.ten = ten;
         this.queQuan = queQuan;
         this.hinh = hinh;
     }
-
-    public String getTen() { return ten; }
-    public String getQueQuan() { return queQuan; }
-    public int getHinh() { return hinh; }
 }
