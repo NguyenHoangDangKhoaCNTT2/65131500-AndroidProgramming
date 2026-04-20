cb20013aa11545069e02bf258f7a00242e7b484d
NguyenHoangDangKhoaCNTT2 <khoa.nhd.65cntt@ntu.edu.vn>
2026-04-20 08:36:44 +0700

    LẦN THỨ 7

--- ThiGK2_NguyenHoangDangKhoa_65131500/app/src/main/java/thigk2/NguyenHoangDangKhoa/ChucNang3.java
+++ ThiGK2_NguyenHoangDangKhoa_65131500/app/src/main/java/thigk2/NguyenHoangDangKhoa/ChucNang3.java
@@ -20,17 +20,15 @@ public class ChucNang3 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chuc_nang3);
 
-        ListView listViewDanhNhan = findViewById(R.id.listViewDanhNhan); // Xem file activity_chuc_nang3.xml có ID này chưa nhé
+        ListView listViewDanhNhan = findViewById(R.id.listViewDanhNhan);
         ArrayList<DanhNhan> list = new ArrayList<>();
 
-        // Truyền thẳng ảnh vào đây (nhớ thay hinh1, hinh2 bằng tên ảnh thật của mày trong drawable)
-        list.add(new DanhNhan("Nguyễn Du", "Nam Định", R.drawable.hinh1));
+        list.add(new DanhNhan("Trần Hưng Đạo", "Nam Định", R.drawable.hinh1));
         list.add(new DanhNhan("Nguyễn Trãi", "Hải Dương", R.drawable.hinh2));
-        list.add(new DanhNhan("Nguyễn Đình Chiểu", "Quảng Bình", R.drawable.hinh3));
+        list.add(new DanhNhan("Võ Nguyên Giáp", "Quảng Bình", R.drawable.hinh3));
         list.add(new DanhNhan("Quang Trung", "Bình Định", R.drawable.hinh4));
         list.add(new DanhNhan("Hồ Chí Minh", "Nghệ An", R.drawable.hinh5));
 
-        // Viết gộp Adapter ngay tại đây luôn cho lẹ
         ArrayAdapter<DanhNhan> adapter = new ArrayAdapter<DanhNhan>(this, R.layout.item_danh_nhan, list) {
             @NonNull
             @Override
@@ -41,12 +39,12 @@ public class ChucNang3 extends AppCompatActivity {
 
                 TextView txtTen = convertView.findViewById(R.id.txtTen);
                 TextView txtQueQuan = convertView.findViewById(R.id.txtQueQuan);
-                ImageView imgHinh = convertView.findViewById(R.id.imgHinh); // Ánh xạ ảnh
+                ImageView imgHinh = convertView.findViewById(R.id.imgHinh);
 
                 DanhNhan dn = list.get(position);
                 txtTen.setText(dn.ten);
                 txtQueQuan.setText("Quê quán: " + dn.queQuan);
-                imgHinh.setImageResource(dn.hinh); // Set ảnh thật vào đây!
+                imgHinh.setImageResource(dn.hinh);
 
                 return convertView;
             }
